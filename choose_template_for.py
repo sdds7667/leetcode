a009e816fb0151688adf9a85ca1065e33b459424
Plămădeală Ion <inplmd@gmail.com>
2022-08-25 19:26:42 +0000

    feat(random-template-picker): implements the random template picker

---
+++ choose_template_for.py
@@ -0,0 +1,24 @@
+import random
+from pathlib import Path
+from argparse import ArgumentParser
+import shutil
+import os
+
+if __name__ == "__main__":
+    arguments = ArgumentParser(
+        description="Randomly picks a template for the problem and initializes the proper template")
+
+    arguments.add_argument("problem-number", type=int,
+                           help="The number of the problem to create the template for")
+    probNumber = arguments.parse_args().__dict__['problem-number']
+    probPath = Path(f"prob{probNumber}")
+    if (probPath.exists()):
+        print("Already exists; Removing")
+        shutil.rmtree(probPath)
+
+    shutil.copytree((choice := random.choice(
+        list(Path("./templates").iterdir()))), probPath)
+
+    print(f"Chosen {choice.name}")
+    os.system(
+        f"git checkout -b prob/{probNumber} && git add . && git commit -am \"prob({probNumber}): Adds the problem skeleton in {choice.name}\"")
