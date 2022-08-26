620e44555b06fb7ab4dedfe9a24b578431536275
Plămădeală Ion <inplmd@gmail.com>
2022-08-26 12:47:51 +0000

    prob(42): dp -- r[i] = max until end, l[i] = max from start; v[i] = min(l[i],r[i])-h[i])

--- prob42/main.cpp
+++ prob42/main.cpp
@@ -11,11 +11,18 @@ public:
         cin.tie(0);
         cout.tie(0);
     }
-    
+    int trap(vector<int>& height) {
+        vector<int> r (height.size());
+        for(int i = height.size() - 1, m = 0; i >= 0; i--) r[i] = (m = max(m, height[i]));
+        int s = 0;
+        for(int i = 0, m=height[0], c=height[0]; i < height.size(); c = height[++i]) 
+            s += min((m = max(m,c)), r[i]) - c;
+        return s;
+    }
 };
 
-bool test(UNDEFINED values, int expected) {
-    auto result = (new Solution())->;
+bool test(vector<int>& values, int expected) {
+    auto result = (new Solution())->trap(values);
     if (result != expected) {
         cout << "Got " << result << " expected " << expected << "\n";
         cout << "Failed test\n";
@@ -30,6 +37,8 @@ bool test(UNDEFINED values, int expected) {
 int main() {
     cout << "Running" << "\n";
     vector<pair<vector<int> *, int>> tests {
+        make_pair(new vector<int>{0,1,0,2,1,0,1,3,2,1,2,1}, 6),
+        make_pair(new vector<int>{4,2,0,3,2,5}, 9)
     };
 
     bool failed = false;
