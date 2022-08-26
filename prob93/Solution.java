496dcb2b96d9e17d67e81074c751f83566489e81
Plămădeală Ion <inplmd@gmail.com>
2022-08-26 08:46:54 +0000

    prob(93): recursive solution

--- prob93/Solution.java
+++ prob93/Solution.java
@@ -1,6 +1,52 @@
+import java.util.*;
+
 class Solution {
     public List<String> restoreIpAddresses(String s) {
+        List<String> ls = new ArrayList<String>();
+        r(s.toCharArray(), ls, new Stack<String>(), 0, 4);
+        return ls;
+    }
+
+    public void r(char[] s, List<String> rs, Stack<String> ss, int i, int c) {
+        var cl = s.length -i;
+        if (c == 1) {
+            if (cl > 3 || cl == 0) return;
+            if (cl == 3 && s[i] > '2') return;
+            if (s[i] == '0' && cl != 1) return;
+            var sb = new StringBuilder();
+            for(; i < s.length; i++) sb.append(s[i]);
+
+            var fs = new StringBuilder();
+            for(var j: ss) {
+                fs.append(j);
+                fs.append('.');
+            }
+            fs.append(sb.toString());
+            rs.add(fs.toString());
+            return;
+        }
+
+        if (cl > c * 3 || (cl < c)) 
+            return;
+
+        var sb = new StringBuilder();
+        sb.append(s[i]);
+        ss.push(sb.toString());
+        r(s, rs, ss, i+1, c-1);
+        ss.pop();
         
+        if (s[i] == '0') return;
+        sb.append(s[i + 1]);
+        ss.push(sb.toString());
+        r(s, rs, ss, i+2, c-1);
+        ss.pop();
+
+        if (s[i] > '2') return;
+
+        sb.append(s[i + 2]);
+        ss.push(sb.toString());
+        r(s, rs, ss, i + 3, c - 1);
+        ss.pop();
     }
     
 }
--- prob93/SolutionTest.java
+++ prob93/SolutionTest.java
@@ -5,20 +5,23 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
 
 class SolutionTest {
 
     public static Stream<Arguments> leetCode() {
         return Stream.of(
                 Arguments.of("25525511135", List.of("255.255.11.135","255.255.111.35")),
-                Arguments.of("0000", List.of("0.0.0.0"))
+                Arguments.of("0000", List.of("0.0.0.0")),
+                Arguments.of("25525511335", List.of("255.255.113.35")),
+                Arguments.of("255514342", List.of("25.55.143.42","255.5.143.42","255.51.43.42")),
+                Arguments.of("25514342", List.of("2.55.143.42","25.5.143.42","25.51.43.42","255.1.43.42","255.14.3.42","255.14.34.2","255.143.4.2"))
         );
     }
 
     @ParameterizedTest
     @MethodSource("leetCode")
-    public void test(int x, int y, int expected) {
-        System.out.println("Testing");
-        assertEquals(expected, new Solution().add(x, y));
+    public void test(String s, List<String> expected) {
+        assertEquals(expected, new Solution().restoreIpAddresses(s));
     }
 }
