01756f04e2d80839f834117316a48f2d18cbeea0
Plămădeală Ion <inplmd@gmail.com>
2022-08-27 22:07:59 +0000

    prob(363): dp -- prefix sum/brute force

--- prob363/Solution.java
+++ prob363/Solution.java
@@ -1,5 +1,28 @@
 class Solution {
-    public int add(int x, int y) {
-        return x+y;
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        int r = matrix.length, c = matrix[0].length;
+        int[][] ps = new int[r+1][c+1];
+
+        for(var i = 1; i <= r; i++) {
+            int cs = 0;
+            for(int j = 1; j <= c; j++) {
+                cs += matrix[i-1][j-1] ;
+                ps[i][j] = cs + ps[i-1][j];
+                if (ps[i][j] == k) 
+                    return k;
+                
+            }
+        }
+
+       
+        int s;
+        int b = Integer.MIN_VALUE;
+        for(var i = 0; i < r; i++) 
+            for(var j = 0; j < c; j++) 
+                for(var m = i+1; m <= r; m++) 
+                    for(var l = j+1; l <= c; l++) 
+                        if ((s = (ps[m][l] - ps[i][l] - ps[m][j] + ps[i][j])) == k) return k;
+                        else if (s < k) b = Math.max(b, s);
+        return b;
     }
 }
--- prob363/SolutionTest.java
+++ prob363/SolutionTest.java
@@ -10,15 +10,13 @@ class SolutionTest {
 
     public static Stream<Arguments> leetCode() {
         return Stream.of(
-                Arguments.of(1, 2, 3),
-                Arguments.of(-1, 1, 0)
+                Arguments.of(new int[][]{new int[]{1,0,1},{0,2,-3}},2,2)
         );
     }
 
     @ParameterizedTest
     @MethodSource("leetCode")
-    public void test(int x, int y, int expected) {
-        System.out.println("Testing");
-        assertEquals(expected, new Solution().add(x, y));
+    public void test(int[][] n, int k, int expected) {
+        assertEquals(expected, new Solution().maxSumSubmatrix(n, k));
     }
 }
