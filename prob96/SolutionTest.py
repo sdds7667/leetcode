807d170eedd56aec191d87bb94fd40e8837a5dd7
Plămădeală Ion <inplmd@gmail.com>
2022-08-29 12:55:42 +0000

    prob(96): dp

--- prob96/SolutionTest.py
+++ prob96/SolutionTest.py
@@ -7,4 +7,5 @@ from Solution import Solution
 class SolutionTest(TestCase):
 
     def test_sum(self):
-        self.assertEqual(3, Solution().sum(1, 2))
+        print("Tests not provided")
+        return 
