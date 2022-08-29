00309633943c2d451e9993ba3c5e31b0b2752f01
Plămădeală Ion <inplmd@gmail.com>
2022-08-29 12:31:14 +0000

    prob(264): dp with pq

--- prob264/Solution.py
+++ prob264/Solution.py
@@ -1,6 +1,27 @@
 from typing import List
-
+from heapq import *
 
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        return 0;
+        un = [1]
+        n -= 1
+        s = set()
+        while n != 0:
+            n -= 1
+            i = heappop(un)
+            k = i * 2
+            if (k not in s):
+                s.add(k)
+                heappush(un, k)
+            
+            k = i * 3
+            if (k not in s):
+                s.add(k)
+                heappush(un, k)
+            k = i * 5
+            if (k not in s):
+                s.add(k)
+                heappush(un, k)
+        return un[n]
+
+
--- prob264/SolutionTest.py
+++ prob264/SolutionTest.py
@@ -7,5 +7,5 @@ from Solution import Solution
 class SolutionTest(TestCase):
 
     def test_sum(self):
-        self.assertEqual(10, 12)
-        self.assertEqual(1,1)
+        self.assertEqual(12, Solution().nthUglyNumber(10))
+        self.assertEqual(1, Solution().nthUglyNumber(1))
