1d400f29722739838fdf1fe1cc97873916952c28
Plămădeală Ion <inplmd@gmail.com>
2022-08-30 08:34:32 +0000

    prob(931): classic dp

--- prob931/Solution.java
+++ prob931/Solution.java
@@ -1,5 +1,22 @@
 class Solution {
-    public int add(int x, int y) {
-        return x+y;
+    public int minFallingPathSum(int[][] matrix) {
+        // dp, o(i) = min(o[i+1][j+1], o[i+1][j], o[i+1][j+1])
+        // return min(o[0])
+        
+        int r = matrix.length;
+        int c = matrix[0].length;
+        var dp = new int[r][c];
+        for(var j = 0; j < c; j++) 
+            dp[r-1][j] = matrix[r-1][j];
+
+        for(var i = r-2; i >= 0; i--)
+            for(var j = 0; j < c; j++) 
+                dp[i][j] = matrix[i][j] + Math.min((j == 0) ? Integer.MAX_VALUE : dp[i+1][j-1], Math.min((j == c - 1) ? Integer.MAX_VALUE : dp[i+1][j+1], dp[i+1][j]));
+
+        int m = Integer.MAX_VALUE;
+        for(var j = 0; j < c; j++) 
+            m = Math.min(m, dp[0][j]);
+        return m;
+
     }
 }
