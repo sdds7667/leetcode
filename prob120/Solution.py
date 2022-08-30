b6f956c11691430ee78dc85dacfcb8e32eb087af
Plămădeală Ion <inplmd@gmail.com>
2022-08-30 08:42:11 +0000

    prob(120): classical dp

--- prob120/Solution.py
+++ prob120/Solution.py
@@ -2,5 +2,12 @@ from typing import List
 
 
 class Solution:
-    def sum(self, x: int, y: int) -> int:
-        return x + y
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        for i in range(1, len(triangle)):
+            p = triangle[i-1]
+            t = triangle[i]
+            lt = len(t)
+            for j in range(lt):
+                t[j] += p[j] if j == 0 else (p[j - 1]
+                                             if j == lt-1 else min(p[j], p[j-1]))
+        return min(triangle[-1])
--- prob120/SolutionTest.py
+++ prob120/SolutionTest.py
@@ -7,4 +7,6 @@ from Solution import Solution
 class SolutionTest(TestCase):
 
     def test_sum(self):
-        self.assertEqual(3, Solution().sum(1, 2))
+        self.assertEqual(11, Solution().minimumTotal(
+            [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]))
+        self.assertEqual(-10, Solution().minimumTotal([[-10]]))
