bc002bad51f9e04864f00c1fceeb08dc7425768d
Plămădeală Ion <inplmd@gmail.com>
2022-09-04 09:27:49 +0000

    prob(64): dp

--- prob64/Solution.py
+++ prob64/Solution.py
@@ -2,5 +2,31 @@ from typing import List
 
 
 class Solution:
-    def sum(self, x: int, y: int) -> int:
-        return x + y
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        r = len(grid)
+        c = len(grid[0])
+        dp = [[0 for i in range(c)] for i in range(r)]
+        prev = grid[r-1][c-1]
+        dp[r-1][c-1] = prev
+
+        j = c-2
+        while j >= 0:
+            prev += grid[r-1][j] 
+            dp[r-1][j] = prev
+            j -= 1
+
+        i = r - 2
+        prev = dp[r-1][c-1]
+        while i >= 0:
+            prev += grid[i][c-1]
+            dp[i][c-1] = prev
+
+        i = r - 2
+        while(i >= 0):
+            j = c-2
+            while(j >= 0):
+                dp[i][j] = max(dp[i+1][j], dp[i][j+1]) + grid[i][j]
+                j -= 1
+            i -= 1
+        
+        return dp[0][0]
--- prob64/SolutionTest.py
+++ prob64/SolutionTest.py
@@ -7,4 +7,5 @@ from Solution import Solution
 class SolutionTest(TestCase):
 
     def test_sum(self):
-        self.assertEqual(3, Solution().sum(1, 2))
+        self.assertEqual(7, Solution().minPathSum([[1,3,1],[1,5,1],[4,2,1]]))
+        self.assertEqual(12, Solution().minPathSum([[1,2,3],[4,5,6]]))
