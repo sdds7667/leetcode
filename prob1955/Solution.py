9c9d4f5296ab77ef58397ebf575cc593ac369055
Plămădeală Ion <inplmd@gmail.com>
2022-09-19 11:55:26 +0000

    prob(1955): start

--- prob1955/Solution.py
+++ prob1955/Solution.py
@@ -2,5 +2,18 @@ from typing import List
 
 
 class Solution:
-    def sum(self, x: int, y: int) -> int:
-        return x + y
+    def countSpecialSubsequences(self, nums: List[int]) -> int:
+        x = 0
+        for i in range(nums):
+            x += self.recursive_solution(nums, -1, i)
+        return x
+
+    def recursive_solution(self, nums: List[int], highest: int, i: int) -> int:
+        # take the current number
+        if highest == 2: return 1
+        x = 0
+        for j in range(i, len(nums)):
+            if nums[j] == highest + 1:
+                x += self.recursive_solution(nums, highest + 1, j)
+        return x
+
