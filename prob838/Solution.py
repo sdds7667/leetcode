d37ffac5ba483834b2771c6e9919b3490cf776f8
Plămădeală Ion <inplmd@gmail.com>
2022-09-28 06:34:03 +0000

    prob(838): dp

--- prob838/Solution.py
+++ prob838/Solution.py
@@ -1,6 +1,36 @@
+import enum
 from typing import List
 
 
 class Solution:
-    def sum(self, x: int, y: int) -> int:
-        return x + y
+    def pushDominoes(self, dominoes: str) -> str:
+        dl = [0] * len(dominoes)
+        cdl = 100000
+        j = len(dominoes) - 1
+        for i in reversed(dominoes):
+            if i == 'L':
+                cdl = 0
+            elif i == 'R':
+                cdl = 100000
+            elif i == '.':
+                cdl = min(cdl + 1, 100000)
+            dl[j] = cdl
+            j -= 1
+        
+        r = []
+        cdr = 100000
+        for ind, i in enumerate(dominoes):
+            if i == 'R':
+                cdr = 0
+            elif i == 'L':
+                cdr = 100000
+            else:
+                cdr = min(cdr+1, 100000)
+            if dl[ind] == cdr:
+                r.append('.')
+            elif dl[ind] > cdr:
+                r.append('R')
+            else:
+                r.append('L')
+
+        return "".join(r)
--- prob838/SolutionTest.py
+++ prob838/SolutionTest.py
@@ -6,5 +6,9 @@ from Solution import Solution
 
 class SolutionTest(TestCase):
 
-    def test_sum(self):
-        self.assertEqual(3, Solution().sum(1, 2))
+    # def test_sum(self):
+    #     self.assertEqual("RR.L", Solution().pushDominoes("RR.L"))
+
+    def test_leetcode(self):
+        self.assertEqual("LL.RR.LLRRLL..",
+                         Solution().pushDominoes(".L.R...LR..L.."))
