997c6f9f3af2e82a519f47530e1052a1ddd513a1
Plămădeală Ion <inplmd@gmail.com>
2022-09-28 06:39:36 +0000

    prob(19): slow, fast pointer

--- prob19/Solution.java
+++ prob19/Solution.java
@@ -1,5 +1,23 @@
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
 class Solution {
-    public int add(int x, int y) {
-        return x+y;
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head.next == null) return null;
+        var fast = head;
+        for(; n>0;n--) fast = fast.next;
+        var slow = head;
+        if (fast == null) return head.next;
+        for(;fast.next != null; fast = fast.next, slow = slow.next);
+        if (slow.next.next == null) 
+            slow.next = null;
+        else
+            slow.next = slow.next.next;
+        return head;
     }
 }
