b95737445e7a8138d84b78f1d0a35978fe7a94e2
Plămădeală Ion <inplmd@gmail.com>
2022-09-30 15:23:26 +0000

    prob(658): l/r pointer

--- prob658/main.cpp
+++ prob658/main.cpp
@@ -11,39 +11,24 @@ public:
         cin.tie(0);
         cout.tie(0);
     }
+
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int l = 0;
+        int r = arr.size() - 1;
+
+        for(int cs = arr.size();cs > k;cs--) 
+            if (abs(arr[l] - x) <= abs(arr[r] - x)) 
+                r--;
+            else
+                l++;
+        
+        auto li = arr.begin() + l;
+        auto ri = arr.begin() + r + 1;
+        return vector<int>(li, ri);
+    }
     
 };
 
-bool test(UNDEFINED values, int expected) {
-    auto result = (new Solution())->;
-    if (result != expected) {
-        cout << "Got " << result << " expected " << expected << "\n";
-        cout << "Failed test\n";
-        for(const auto i: values)
-            cout << i << " ";
-        cout << "\n";
-        return false;
-    }
-    return true;
-}
-
 int main() {
-    cout << "Running" << "\n";
-    vector<pair<vector<int> *, int>> tests {
-    };
-
-    bool failed = false;
-
-    for(const auto& p : tests) {
-        if (!test((*p.first), p.second)) {
-            failed = true;
-            break;
-        }
-    }
-
-    if (!failed) 
-        cout << "Passed all tests\n";
-
-    cout << "Done running.\n";
-    return 0;
+    // no tests
 }
