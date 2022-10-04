6d102714828aed950e5e01a80f44f036aa28423b
Plămădeală Ion <inplmd@gmail.com>
2022-10-04 10:39:16 +0000

    prob(589): dfs

--- prob589/main.cpp
+++ prob589/main.cpp
@@ -4,46 +4,42 @@
 using namespace std;
 
 
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+
 class Solution {
 public:
+
+    vector<int> res;
     Solution() {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
     }
+
+    vector<int> preorder(Node* root) {
+        if (root == nullptr) return;
+        res.push_back(root->val);
+        for(auto* r: root->children)
+            preorder(r);
+        return res;
+    }
     
 };
 
-bool test(UNDEFINED values, int expected) {
-    auto result = (new Solution())->;
-    if (result != expected) {
-        cout << "Got " << result << " expected " << expected << "\n";
-        cout << "Failed test\n";
-        for(const auto i: values)
-            cout << i << " ";
-        cout << "\n";
-        return false;
-    }
-    return true;
-}
-
-int main() {
-    cout << "Running" << "\n";
-    vector<pair<vector<int> *, int>> tests {
-    };
-
-    bool failed = false;
-
-    for(const auto& p : tests) {
-        if (!test((*p.first), p.second)) {
-            failed = true;
-            break;
-        }
-    }
-
-    if (!failed) 
-        cout << "Passed all tests\n";
-
-    cout << "Done running.\n";
-    return 0;
-}
+int main() {}
