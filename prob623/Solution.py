d5e9c0b80290114158c62ee26d1f96f9af575a12
Plămădeală Ion <inplmd@gmail.com>
2022-10-05 07:47:00 +0000

    prob(623): bfs without deque (too slow)

--- prob623/Solution.py
+++ prob623/Solution.py
@@ -1,6 +1,34 @@
-from typing import List
+from typing import List, Optional
+from collections import deque
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution:
-    def sum(self, x: int, y: int) -> int:
-        return x + y
+    def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
+        if depth == 1:
+            return TreeNode(val, root)
+        
+        bfs_queue = [root]
+        cd = 2
+
+        while cd < depth:
+            cd += 1
+            tmp = []
+            for el in bfs_queue:
+                if el.left is not None:
+                    tmp.append(el.left)
+                if el.right is not None:
+                    tmp.append(el.right)
+            bfs_queue = tmp
+        
+        for el in bfs_queue:
+            el.left = TreeNode(val, el.left)
+            el.right = TreeNode(val, None, el.right)
+
+        return root
+
+
