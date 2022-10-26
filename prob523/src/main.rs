43667e660216a6fc66c8101454ca23f594e964a1
Plămădeală Ion <inplmd@gmail.com>
2022-10-26 21:36:37 +0000

    prob(523): leetcode solution

--- prob523/src/main.rs
+++ prob523/src/main.rs
@@ -1,5 +1,29 @@
 struct Solution {}
 
+
+impl Solution {
+    pub fn check_subarray_sum(nums: Vec<i32>, k: i32) -> bool {
+        // solution from leetcode
+        let mut cs = 0;
+        let mut h = std::collections::HashMap::new();
+        h.insert(0, 0usize);
+        for (ind, i) in nums.iter().enumerate() {
+            cs += i;
+            match h.entry(cs % k) {
+                std::collections::hash_map::Entry::Occupied(e) => 
+                    if *e.get() < ind {
+                        return true
+                    }
+                std::collections::hash_map::Entry::Vacant(e) => {
+                    e.insert(ind + 1);
+                },
+            }
+        }
+
+        false
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
